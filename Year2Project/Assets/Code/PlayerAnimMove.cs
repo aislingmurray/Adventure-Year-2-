44d2b66eb678141ea14dea570ff97ecf00b99ec3
aislingmurray <aislingmurray700@gmail.com>
2021-11-17 22:58:08 +0000

    Anim part.3

--- Year2Project/Assets/Code/PlayerAnimMove.cs
+++ Year2Project/Assets/Code/PlayerAnimMove.cs
@@ -7,6 +7,7 @@ public class PlayerAnimMove : MonoBehaviour
     [SerializeField] private float speed;
     private Rigidbody2D body;
     private Animator anim;
+    private bool grounded;
 
     private void Start()
     {
@@ -27,7 +28,22 @@ public class PlayerAnimMove : MonoBehaviour
         else if (horizontalInput < -0.01f)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);    
+        if (Input.GetKey(KeyCode.Space)&& grounded)
+            Jump();
+
+        //Animator parameters 
+        anim.SetBool("running", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
     }
-}
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            grounded = true;
+    }
+} 
